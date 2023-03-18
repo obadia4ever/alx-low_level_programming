852a4c17eb535b34f861e2d09eb9d25deeb612fd
Obadia Taiwo <obadia4ever@yahoo.com>
2023-03-18 00:02:54 -0700

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
-* Description : main - Prints out a string to stdout.
-* Return: 0 if success.
+ * Description : main - Prints out a string to stdout.
+ * Return: 0 if success.
 */
-int main(void){
-Printf("with proper grammar, but the outcome is a piece of art,");
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return(0);
 }
